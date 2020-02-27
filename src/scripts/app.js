d7a58013e7ffcb9d3bff01e34919e671851b4683
IvanShishkin <i.shishkin@intensa.ru>
2020-02-27 12:31:44 +0300

    init

---
+++ src/scripts/app.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import Test from 'vue/Test.vue';
+
+new Vue({
+    el: '#vue',
+    render: h => h(Test)
+});
---
+++ src/vue/Test.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>{{title}}</h1>
+</template>
+
+<script>
+//import axios from 'axios';
+export default {
+  name: "Test",
+  data() {
+      return {
+          title: 'Vue Test'
+      }
+  },
+  computed:  {
+
+  },
+  mounted : function() {
+
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+  h1 {
+    color: #f00;
+  }
+</style>
